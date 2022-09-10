265d58c304eab1816e5e8b64549bcff8ae2a5bfd
enginooby <enginooby@gmail.com>
2022-09-10 17:49:33 +0200

    pet: Tune attachment params, implement unequip

--- src/ServerStorage/ServerModules/PetModule.lua
+++ src/ServerStorage/ServerModules/PetModule.lua
@@ -2,7 +2,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Workspace = game:GetService("Workspace")
 local module = {}
 
-function module.EquipPet(player: Player, petName)
+function module.EquipPet(player: Player, petName: string)
   local petModel:Instance = ReplicatedStorage.Pets:FindFirstChild(petName)
   if not player or not petModel then return end
 
@@ -11,6 +11,8 @@ function module.EquipPet(player: Player, petName)
 
   petModel=petModel:Clone()
   petModel.Parent=workspace.Pets
+  petModel.PrimaryPart.CFrame=player.Character.HumanoidRootPart.CFrame
+  petModel.PrimaryPart:SetNetworkOwner(player)
 
   local attachment0 = Instance.new("Attachment", petModel.PrimaryPart)
   attachment0.Name="Attachment0"
@@ -23,17 +25,18 @@ local alignPosition:AlignPosition= Instance.new("AlignPosition", petModel.Primar
   alignPosition.Attachment0=attachment0
   alignPosition.Attachment1=attachment1
   alignPosition.RigidityEnabled=false
-  alignPosition.MaxForce=100000
-  alignPosition.Responsiveness=25
+  alignPosition.MaxForce=25000
+  alignPosition.Responsiveness=5
 
   local alignRotation:AlignOrientation= Instance.new("AlignOrientation", petModel.PrimaryPart)
   alignRotation.Attachment0=attachment0
   alignRotation.Attachment1=attachment1
   alignRotation.RigidityEnabled=false
-  alignRotation.MaxTorque=100000 
-  alignRotation.Responsiveness=25
-end 
+  alignRotation.MaxTorque=25000 
+  alignRotation.Responsiveness=5
+end
 
+-- UTIL
 function getOrCreateChild(player: Player, childName: string) : Instance
   if not player.Character.HumanoidRootPart:FindFirstChild(childName) then
     local child = Instance.new("Folder", player.Character.HumanoidRootPart)
@@ -43,11 +46,13 @@ function getOrCreateChild(player: Player, childName: string) : Instance
   return player.Character.HumanoidRootPart:FindFirstChild(childName);
 end
 
-function module.UnequipPet(player)
+function module.UnequipPet(player: Player, petName: string)
   if not player then return end
+
+  workspace.Pets:FindFirstChild(petName):Destroy()
 end
 
-function module.UnequipAllPets(player)
+function module.UnequipAllPets(player: Player)
   if not player then return end
 end
 
