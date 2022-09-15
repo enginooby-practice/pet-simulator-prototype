11673249665545ad5dfc9fca3f1a1490c1eb00f8
enginooby <enginooby@gmail.com>
2022-09-15 12:13:12 +0200

    quest - Create Quest base class

---
+++ src/ReplicatedStorage/CommonModules/Quest.lua
@@ -0,0 +1,72 @@
+local questPrefab = game.ReplicatedStorage:WaitForChild('Quest')
+
+Quest = {}
+Quest.__index = Quest
+
+function Quest.new(
+    position: Vector3,
+    title: string,
+    description: string,
+    timeLimit: number,
+    expReward: number,
+    expPenalty: number,
+    goldReward: number
+)
+    local self = {}
+    setmetatable(self, Quest)
+
+    self.title = title
+    self.description = description
+    self.timeLimit = timeLimit
+    self.expReward = expReward
+    self.expPenalty = expPenalty
+    self.goldReward = goldReward
+    self.isCompleted = false
+
+    local model: Model = questPrefab:Clone()
+    model.Parent = game.Workspace.Quests
+    model:MoveTo(position)
+
+    local clickDetector = Instance.new('ClickDetector', model)
+    clickDetector.MaxActivationDistance = 15
+    clickDetector.MouseClick:Connect(function(player: Player)
+        self:SetTaker(player, nil)
+        self:Show()
+    end)
+
+    return self
+end
+
+function Quest:SetTaker(player: Player, pet)
+    self.player = player
+    self.pet = pet
+end
+
+function Quest:Show()
+    print('>>> Quest: ' .. self.title)
+    print('>>> Content: ' .. self.description)
+end
+
+function Quest:Hide() end
+
+function Quest:Decline() end
+
+function Quest:Start()
+    task.wait(self.timeLimit)
+
+    if not self.isCompleted then
+        self:Fail()
+    end
+end
+
+function Quest:Fail()
+    self.pet:AddExp(self.expPenalty)
+end
+
+function Quest:Complete()
+    self.isCompleted = true
+    self.player.leaderstats.Gold.Value += self.goldReward
+    self.pet:AddExp(self.expReward)
+end
+
+return Quest
---
+++ src/ServerScriptService/Server/QuestManager.server.lua
@@ -0,0 +1,30 @@
+local Quest = require(game.ReplicatedStorage.CommonModules.Quest)
+
+local SPAWN_AMOUNT = 3
+local SPAWN_FREQUENCY = 3 * 60
+
+-- UTIL
+-- ! Must define local function before use
+local function DestroyAllChildren(folder: Folder)
+    for _, instance: Instance in pairs(folder:GetChildren()) do
+        if instance then
+            instance:Destroy()
+        end
+    end
+end
+
+local function SpawnQuest()
+    local randomPosition = Vector3.new(math.random(-15, 112), 30.649, math.random(-27, 85)) -- REFACTOR
+    local quest = Quest.new(randomPosition, 'Quest ' .. math.random(1, 1000), 'Description', 60, 30, 10, 20)
+end
+
+while true do
+    -- PERF: Use pool
+    DestroyAllChildren(game.Workspace.Quests)
+
+    for i = 1, SPAWN_AMOUNT, 1 do
+        SpawnQuest()
+    end
+
+    task.wait(SPAWN_FREQUENCY)
+end
